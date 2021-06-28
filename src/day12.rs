5e30e1216a61a767ecaaa18e24158326317e6511
Clint Olson <coandco@gmail.com>
2021-06-28 10:46:09 -0700

    Rustfmt day 12

--- src/day12.rs
+++ src/day12.rs
@@ -8,7 +8,7 @@ pub enum Op {
     W,
     R,
     L,
-    F
+    F,
 }
 
 impl Op {
@@ -21,7 +21,7 @@ impl Op {
             'R' => Some(Self::R),
             'L' => Some(Self::L),
             'F' => Some(Self::F),
-            _ => None
+            _ => None,
         }
     }
 }
@@ -31,7 +31,7 @@ pub enum Heading {
     N,
     E,
     S,
-    W
+    W,
 }
 
 impl Heading {
@@ -41,7 +41,7 @@ impl Heading {
             Op::E => Heading::E,
             Op::S => Heading::S,
             Op::W => Heading::W,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 
@@ -53,7 +53,7 @@ impl Heading {
                 Heading::N => Heading::E,
                 Heading::E => Heading::S,
                 Heading::S => Heading::W,
-                Heading::W => Heading::N
+                Heading::W => Heading::N,
             }
         }
         new_heading
@@ -67,7 +67,7 @@ impl Heading {
                 Heading::N => Heading::W,
                 Heading::E => Heading::N,
                 Heading::S => Heading::E,
-                Heading::W => Heading::S
+                Heading::W => Heading::S,
             }
         }
         new_heading
@@ -88,19 +88,26 @@ impl Coord {
 
     fn move_direction(&self, direction: &Heading, amount: i32) -> Self {
         let direction_unit = match direction {
-            Heading::N => Coord{y:-1, x:0},
-            Heading::E => Coord{y:0, x:1},
-            Heading::S => Coord{y:1, x:0},
-            Heading::W => Coord{y:0, x:-1}
+            Heading::N => Coord { y: -1, x: 0 },
+            Heading::E => Coord { y: 0, x: 1 },
+            Heading::S => Coord { y: 1, x: 0 },
+            Heading::W => Coord { y: 0, x: -1 },
         };
-        *self + Coord{y: direction_unit.y * amount, x: direction_unit.x * amount}
+        *self
+            + Coord {
+                y: direction_unit.y * amount,
+                x: direction_unit.x * amount,
+            }
     }
 
     #[inline]
     fn rotate_right(&self, times: i32) -> Coord {
         let mut new_point: Coord = *self;
         for _ in 0..(times as usize) {
-            new_point = Coord{x:-new_point.y, y:new_point.x}
+            new_point = Coord {
+                x: -new_point.y,
+                y: new_point.x,
+            }
         }
         new_point
     }
@@ -109,7 +116,10 @@ impl Coord {
     fn rotate_left(&self, times: i32) -> Coord {
         let mut new_point: Coord = *self;
         for _ in 0..(times as usize) {
-            new_point = Coord{x:new_point.y, y:-new_point.x}
+            new_point = Coord {
+                x: new_point.y,
+                y: -new_point.x,
+            }
         }
         new_point
     }
@@ -135,55 +145,59 @@ impl AddAssign for Coord {
 }
 
 pub fn generator(input: &str) -> Vec<(Op, i32)> {
-    input.lines().map(|line| {
-        let op = Op::from(line.chars().next().unwrap()).expect("Invalid op!");
-        let value = &line[1..].parse::<i32>().expect("Couldn't parse value!");
-        (op, *value)
-    }).collect()
+    input
+        .lines()
+        .map(|line| {
+            let op = Op::from(line.chars().next().unwrap()).expect("Invalid op!");
+            let value = &line[1..].parse::<i32>().expect("Couldn't parse value!");
+            (op, *value)
+        })
+        .collect()
 }
 
 pub fn part_one(data: &Vec<(Op, i32)>) -> i32 {
-    let mut current_loc = Coord{x:0, y:0};
+    let mut current_loc = Coord { x: 0, y: 0 };
     let mut current_heading = Heading::E;
     for (op, value) in data {
         match op {
-            Op::N|Op::S|Op::E|Op::W => {
+            Op::N | Op::S | Op::E | Op::W => {
                 current_loc = current_loc.move_direction(&Heading::from(op), *value);
-            },
+            }
             Op::R => {
                 let num_turns = (value / 90) % 4;
                 current_heading = current_heading.turn_right(num_turns);
-            },
+            }
             Op::L => {
                 let num_turns = (value / 90) % 4;
-                current_heading = current_heading.turn_left( num_turns);
-            },
-            Op::F => {
-                current_loc = current_loc.move_direction(&current_heading, *value)
+                current_heading = current_heading.turn_left(num_turns);
             }
+            Op::F => current_loc = current_loc.move_direction(&current_heading, *value),
         }
     }
     current_loc.manhattan_distance()
 }
 
 pub fn part_two(data: &Vec<(Op, i32)>) -> i32 {
-    let mut ship_loc = Coord{x:0, y:0};
-    let mut waypoint_loc = Coord{y:-1, x:10};
+    let mut ship_loc = Coord { x: 0, y: 0 };
+    let mut waypoint_loc = Coord { y: -1, x: 10 };
     for (op, value) in data {
         match op {
-            Op::N|Op::S|Op::E|Op::W => {
+            Op::N | Op::S | Op::E | Op::W => {
                 waypoint_loc = waypoint_loc.move_direction(&Heading::from(op), *value);
-            },
+            }
             Op::R => {
                 let num_rotations = (value / 90) % 4;
                 waypoint_loc = waypoint_loc.rotate_right(num_rotations);
-            },
+            }
             Op::L => {
                 let num_rotations = (value / 90) % 4;
                 waypoint_loc = waypoint_loc.rotate_left(num_rotations);
-            },
+            }
             Op::F => {
-                ship_loc += Coord{y:waypoint_loc.y*value, x:waypoint_loc.x*value}
+                ship_loc += Coord {
+                    y: waypoint_loc.y * value,
+                    x: waypoint_loc.x * value,
+                }
             }
         }
     }
